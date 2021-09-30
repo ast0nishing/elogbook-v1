30fd018c88a5449115fcefd09b101b657d3c0b69
tleq <thaile.isme@gmail.com>
2021-09-30 10:50:22 +0700

    update

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,24 +1,9 @@
-// import { Link } from 'react-router-dom';
-
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
     return (
         <header className={classes.header}>
             <div className={classes.logo}>Logbook</div>
-            {/* <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>All Meetups</Link>
-                    </li>
-                    <li>
-                        <Link to='/newmeetup'>New Meetup</Link>
-                    </li>
-                    <li>
-                        <Link to='/favorites'>Favorites</Link>
-                    </li>
-                </ul>
-            </nav> */}
         </header>
     );
 }
