97aa8f3bba9c3b1eb4f272202e6f658edddc891c
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2021-10-04 18:12:41 +0700

    reduce num of tables

--- backend/api/models/HeadTeacher.model.js
+++
@@ -1,9 +0,0 @@
-export default function (sequelize, Sequelize) {
-    const HeadTeacher = sequelize.define('headTeachers', {
-        id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-        name: { type: Sequelize.STRING, allowNull: false },
-        classId: { type: Sequelize.STRING, allowNull: false },
-        academicYearId: { type: Sequelize.STRING, allowNull: false },
-    });
-    return HeadTeacher;
-}
