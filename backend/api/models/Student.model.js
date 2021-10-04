7b49c21a695795eb274334f4e4127a024b270b46
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2021-10-04 18:05:17 +0700

    add

--- backend/api/models/Student.model.js
+++ backend/api/models/Student.model.js
@@ -1,27 +1,27 @@
 export default function (sequelize, Sequelize) {
-    const Student = sequelize.define('students', {
-        name: { type: Sequelize.STRING, allowNull: false, primaryKey: true },
-        address: { type: Sequelize.STRING, allowNull: true },
-        phoneNumber: { type: Sequelize.INTEGER },
-        email: {
-            type: Sequelize.STRING,
-            allowNull: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        dob: {
-            type: Sequelize.STRING,
-            validate: {
-                isDate: true,
-            },
-        },
-        school_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
+  const Student = sequelize.define("students", {
+    name: { type: Sequelize.STRING, allowNull: false },
+    address: { type: Sequelize.STRING, allowNull: true },
+    phoneNumber: { type: Sequelize.INTEGER },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    dob: {
+      type: Sequelize.STRING,
+      validate: {
+        isDate: true,
+      },
+    },
+    school_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
 
-        class_id: { type: Sequelize.INTEGER },
-    });
-    return Student;
+    class_id: { type: Sequelize.INTEGER },
+  });
+  return Student;
 }
--- backend/api/models/index.js
+++ backend/api/models/index.js
@@ -1,41 +1,41 @@
-import { default as dbConfig } from '../db.config.js';
-import { default as User } from './User.model.js';
-import { default as School } from './School.model.js';
-import { default as Timetable } from './Timetable.model.js';
-import { default as Teacher } from './Teacher.model.js';
-import { default as Student } from './Student.model.js';
-import { default as Class } from './Class.model.js';
-import { default as AcademicYear } from './AcademicYear.model.js';
-import { default as Logbook } from './Logbook.model.js';
-import { default as Lesson } from './Lesson.model.js';
-import { default as Absent } from './Absent.model.js';
-import { default as Grade } from './Grade.model.js';
-import { default as HeadTeacher } from './HeadTeacher.model.js';
-import { default as Period } from './Period.model.js';
-import { default as Course } from './Course.model.js';
-import { default as Admin } from './Admin.model.js';
+import { default as dbConfig } from "../db.config.js";
+import { default as User } from "./User.model.js";
+import { default as School } from "./School.model.js";
+import { default as Timetable } from "./Timetable.model.js";
+import { default as Teacher } from "./Teacher.model.js";
+import { default as Student } from "./Student.model.js";
+import { default as Class } from "./Class.model.js";
+import { default as AcademicYear } from "./AcademicYear.model.js";
+import { default as Logbook } from "./Logbook.model.js";
+import { default as Lesson } from "./Lesson.model.js";
+import { default as Absent } from "./Absent.model.js";
+import { default as Grade } from "./Grade.model.js";
+import { default as HeadTeacher } from "./HeadTeacher.model.js";
+import { default as Period } from "./Period.model.js";
+import { default as Course } from "./Course.model.js";
+import { default as Admin } from "./Admin.model.js";
 
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorAliases: false,
-    // logging: false,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle,
-    },
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorAliases: false,
+  // logging: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
 });
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch((err) => {
-        'Unable to connect to the database:', err;
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    "Unable to connect to the database:", err;
+  });
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
@@ -55,10 +55,15 @@ db.teachers = Teacher(sequelize, Sequelize);
 db.timetables = Timetable(sequelize, Sequelize);
 db.users = User(sequelize, Sequelize);
 
-db.students.belongsTo(db.users);
+// db.students.hasOne(db.users);
+// db.users.belongsTo(db.students, { foreignKey: "identity" });
+
+db.students.hasOne(db.users);
+db.users.belongsTo(db.students);
+
 db.teachers.belongsTo(db.users);
-db.admins.belongsTo(db.users);
-db.users.hasOne(db.students);
 db.users.hasOne(db.teachers);
+
+db.admins.belongsTo(db.users);
 db.users.hasOne(db.admins);
 export default db;
